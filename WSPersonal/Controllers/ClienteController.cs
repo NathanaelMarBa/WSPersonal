87be84f7ffd7ce0cf4463aedcd406fc8c4253bc6
Nathanael <Nathanael@DESKTOP-R1I3MRF>
2024-04-10 18:59:31 -0600

    Agregar archivos de proyecto.

---
+++ WSPersonal/Controllers/ClienteController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WSPersonal.Models;
+
+namespace WSPersonal.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        [ActionName("listar")]
+        [HttpGet]
+        public dynamic ListarCliente()
+        {
+            List<Cliente> clientes = new List<Cliente>
+            {
+                new Cliente
+                {
+                    id = "1",
+                    correo = "kevinnathanaelm@gmail.com",
+                    edad = "20",
+                    nombre = "Nathanael Martinez"
+                },
+                new Cliente
+                {
+                    id = "2",
+                    correo = "paty@gmail.com",
+                    edad = "40",
+                    nombre = "Patricia Barrera"
+                }
+            };
+            return clientes;
+        }
+        [ActionName("guardar")]
+        [HttpPost]
+        public dynamic guardarCliente(Cliente cliente)
+        {
+            return new
+            {
+                success = true,
+                message = "Cliente Registrado",
+                result = cliente
+            };
+        }
+    }
+}
---
+++ WSPersonal/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSPersonal.Models
+{
+    public class Cliente
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string edad { get; set; }
+        public string correo { get; set; }
+    }
+}
